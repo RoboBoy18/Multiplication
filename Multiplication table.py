81aa0d46c56ffa6390a3c93a96e91de26aa09f73
Aman Patel <Patelaman101@gmail.com>
2020-10-01 16:01:05 +0530

    Update Multiplication table.py
    I have made this program suitable for user input so that user can input the number of his choice whose table he/she wants to be print.

--- Multiplication table.py
+++ Multiplication table.py
@@ -1,5 +1,6 @@
+number=int(input('Enter the number whose table you want to print up to 10 multiples: '))
 i=1
 while(i<=10):
-    t=2*i
-    print("\n",2,"*",i,"=",t)
+    t=number*i
+    print(f'\n{number} * i = {t}')
     i=i+1
