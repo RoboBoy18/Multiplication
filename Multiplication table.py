3c1669d5e2f518d04454e6f1533ea3546ef3f4a2
Aman Patel <Patelaman101@gmail.com>
2021-10-15 16:04:59 +0530

    Update Multiplication table.py
    I have replaced the f string with the format function for better readability , also  use the shorthand for updating the value of i

--- Multiplication table.py
+++ Multiplication table.py
@@ -1,6 +1,6 @@
-number=int(input('Enter the number whose table you want to print up to 10 multiples: '))
+number=int(input('Enter the number whose multiplication table you want to print up to 10 multiples:'))
 i=1
 while(i<=10):
     t=number*i
-    print(f'\n{number} * i = {t}')
-    i=i+1
+    print('\n{} * i = {}'.format(number,t))
+    i+=1
